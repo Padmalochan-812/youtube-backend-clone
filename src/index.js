d5a47680b731d0199fa622ce7c1714a99a85c49e
Padmalochan-812 <sahoopadma812@gmail.com>
2026-07-10 01:31:44 +0530

    utillis code custom error, response, asyncHandeler create

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ dotenv.config({
 })
 
 connectionDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR is : ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
