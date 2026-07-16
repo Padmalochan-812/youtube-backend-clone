4f4a45051273fe74597de272ffe4c006894663ef
Padmalochan-812 <sahoopadma812@gmail.com>
2026-07-16 13:27:23 +0530

    debug

--- src/app.js
+++ src/app.js
@@ -7,12 +7,22 @@ const app = express()
 
 app.use(cors({
    origin : process.env.CORS_ORIGIN,
-   credential: ture
+   credentials: true
 }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import 
+
+import userRouter from "./routes/user.route.js"
+
+//routes decleration
+app.use("/api/v1/users", userRouter)
+
+//http://localhost:8000/api/v1/users/register
+
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import dotenv from "dotenv"
 import connectionDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 connectionDB()
