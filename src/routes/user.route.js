0a1f0fcd8cb5fd199c3a8ceef46af75c2f0eee7c
Padmalochan-812 <sahoopadma812@gmail.com>
2026-07-23 23:55:34 +0530

    add refereshAccessToken route to user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logoutUser,loginUser, registerUser } from "../controllers/user.controllers.js";
+import { logoutUser,loginUser, registerUser, refereshAccessToken } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js"
 import { veryfyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -25,5 +25,6 @@ router.route("/login").post(loginUser)
 //secured routes 
 
 router.route("/logout").post(veryfyJWT, logoutUser)
+router.route("/referesh-token").post(refereshAccessToken)
 
 export default router ;
