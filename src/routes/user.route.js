c505a0ea2b5257f242540d77488096a03ce2b268
Padmalochan-812 <sahoopadma812@gmail.com>
2026-08-01 12:20:25 +0530

    add user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { logoutUser,loginUser, registerUser, refereshAccessToken } from "../controllers/user.controllers.js";
+import { 
+    logoutUser,
+    loginUser,
+    registerUser, 
+    refereshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controllers.js";
+
 import { upload } from "../middlewares/multer.middlewares.js"
 import { veryfyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -26,5 +39,14 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(veryfyJWT, logoutUser)
 router.route("/referesh-token").post(refereshAccessToken)
+router.route("/change-password").post(veryfyJWT, changeCurrentPassword)
+router.route("/curren-user").get(getCurrentUser)
+router.route("/update-account").patch(updateAccountDetails)
+
+router.route("/avatar").patch(veryfyJWT, upload.single("/avatar"), updateUserAvatar)
+router.route("/cover-image").patch(veryfyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(veryfyJWT, getUserChannelProfile)
+router.route("/history").get(veryfyJWT, getWatchHistory)
 
 export default router ;
